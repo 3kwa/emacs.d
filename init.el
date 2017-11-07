c640c9060d25b596df6c1a661253e555b78db42f
Eugene Van den Bulke <eugvan@optiver.com.au>
2017-11-07 18:31:31 +1100

    getting started

---
+++ init.el
@@ -0,0 +1,16 @@
+(tool-bar-mode -1)
+(load-theme 'desert t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("44010f20429054fcf8842a5c36233f48a8b711b3d00c1b48b3b899306f0912f1" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
